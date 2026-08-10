3f4c622102e0340015a38726b15d7dc31d59fdaa
Priyanshi Patel <pbp1809@gmail.com>
2026-08-10 14:45:03 +0100

    Complete Week 1 student management project

--- student_management.py
+++ student_management.py
@@ -2,8 +2,10 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
+# Load environment variables
 load_dotenv()
 
+# Connect to PostgreSQL
 connection = psycopg2.connect(
     host="localhost",
     database="student_management",
@@ -13,4 +15,38 @@ connection = psycopg2.connect(
 
 print("Database connection successfully!!")
 
+cursor = connection.cursor()
+
+# Get student information
+name = input("Enter student name: ")
+age = int(input("Enter student age: "))
+height = float(input("Enter student height in meters: "))
+city = input("Enter student city: ")
+
+# Insert student into the existing table
+query = """
+    INSERT INTO students (name, age, height, city)
+    VALUES (%s, %s, %s, %s)
+"""
+
+cursor.execute(query, (name, age, height, city))
+
+connection.commit()
+
+print("Student added successfully!!!")
+
+# Display all students
+cursor.execute("SELECT * FROM students")
+
+students = cursor.fetchall()
+
+print("\nStudents:")
+
+for student in students:
+    print(student)
+
+# Close database connection
+cursor.close()
 connection.close()
+
+
---
+++ student_management.sql
@@ -0,0 +1,12 @@
+
+SELECT * FROM public.students
+ORDER BY student_id ASC 
+
+insert into students values 
+(1,'Priya',22,'1.65','London ');
+
+insert into students values
+(2,'Rahul',23,'1.72','Birmingham');
+
+insert into students values
+(3,'Anjali',21,'1.60','Manchester')
